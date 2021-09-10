45ff4c84a51c27ad252db7b574e37763b0c6ffff
radiantly <buildingsomethingfun@gmail.com>
2021-09-11 01:50:25 +0530

    Add basic site

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,118 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Content />
+  <div class="dark-overlay" ref="overlay">
+    <div class="middle">
+      <Content />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Content from "./components/Content.vue";
+import { ref, onMounted, onUnmounted } from "vue";
+import gsap from "gsap";
 
 export default {
   name: "App",
+  setup() {
+    const overlay = ref(null);
+    let clipRight = 0;
+    const setClip = (e) => {
+      clipRight = Math.max(0, document.body.clientWidth - e.clientX);
+      gsap.to(overlay.value, {
+        clipPath: `inset(0 ${clipRight}px 0 0)`,
+      });
+    };
+
+    const setClipOut = () => {
+      const bodyWidth = document.body.clientWidth;
+      if (clipRight > 200 && bodyWidth - clipRight > 200) return;
+      gsap.to(overlay.value, {
+        clipPath: `inset(0 ${clipRight > bodyWidth / 2 ? bodyWidth : 0}px 0 0)`,
+      });
+    };
+
+    onMounted(() => {
+      window.addEventListener("mousemove", setClip);
+      document.body.addEventListener("mouseleave", setClipOut);
+    });
+    onUnmounted(() => window.removeEventListener("mousemove", setClip));
+    return {
+      overlay,
+    };
+  },
   components: {
-    HelloWorld,
+    Content,
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@font-face {
+  font-family: "Bohemian Typewriter";
+  src: url("assets/Bohemian Typewriter.woff2");
+}
+
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  margin: 0;
+  min-height: 100vh;
+}
+
+body {
+  font-family: "Bohemian Typewriter";
+  display: flex;
+  justify-content: center;
+  font-size: 20px;
+  color: #333;
+  background-color: #fff;
+  position: relative;
+}
+
+#app,
+.middle {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.dark-overlay {
+  background-color: #222;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  clip-path: inset(0 0 0 0);
+}
+
+h1 {
+  color: #444;
+}
+
+.dark-overlay h1 {
+  color: #fff;
+}
+
+.table {
+  color: #404040;
+}
+
+.dark-overlay .table {
+  color: #efefef;
+}
+
+@media screen and (max-width: 600px) {
+  body {
+    font-size: 18px;
+  }
 }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,115 @@
+<template>
+  <h1>Ascii @ Amrita</h1>
+  <p>
+    Welcome! We’re the ASCII club of Amrita, and we host various technical and
+    non-technical events and workshops.
+  </p>
+  <p>
+    However, most importantly, the club regulates and enforces the usage of the
+    various ASCII characters in and around the coimbatore campus. These
+    characters can be seen in the table below.
+  </p>
+  <div class="table">
+    <div class="col" v-for="characters in cols" :key="characters">
+      <div>Dec</div>
+      <div>Oct</div>
+      <div>Hex</div>
+      <div class="rt">C</div>
+      <template v-for="charInfo in characters" :key="charInfo">
+        <div class="rt">{{ charInfo.dec }}</div>
+        <div class="rt">{{ charInfo.oct }}</div>
+        <div class="rt">{{ charInfo.hex }}</div>
+        <div class="rt">{{ charInfo.char }}</div>
+      </template>
+    </div>
+  </div>
+  <p>
+    In order to use any one of these characters, please write to us at
+    ascii@cb.amrita.edu. Note that this restriction does not apply to characters
+    outside this range.
+  </p>
+  <p>
+    &copy;2021 ASCII@Amrita<span class="notmobile">. All Rights Reserved.</span>
+  </p>
+</template>
+
+<script>
+import { ref, onMounted, onUnmounted } from "vue";
+
+export default {
+  name: "Content",
+  setup() {
+    const characters = [];
+
+    for (let i = 0; i < 128; i++) {
+      characters.push({
+        dec: i.toString(),
+        oct: i.toString(8),
+        hex: i.toString(16),
+        char: i >= 32 && i <= 127 ? String.fromCharCode(i) : "NA",
+      });
+    }
+    const cols = ref([characters]);
+    const year = ref(6666);
+
+    const setCols = () => {
+      const pageWidth = document.body.clientWidth;
+      const colCount = pageWidth >= 700 ? 4 : pageWidth >= 500 ? 3 : 2;
+      const newCols = [];
+      const rows = Math.ceil(characters.length / colCount);
+      for (let i = 0; i < colCount; i++) {
+        newCols.push(characters.slice(i * rows, (i + 1) * rows));
+      }
+      cols.value = newCols;
+    };
+    onMounted(() => {
+      window.addEventListener("resize", setCols);
+      year.value = new Date().getFullYear();
+      setCols();
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("resize", setCols);
+    });
+
+    return {
+      cols,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 400;
+  font-size: 72px;
+  text-align: center;
+}
+
+p {
+  max-width: min(700px, 95vw);
+  text-align: center;
+}
+
+.table {
+  display: flex;
+  justify-content: space-evenly;
+  align-self: stretch;
+}
+.col {
+  display: grid;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+}
+.rt {
+  text-align: right;
+}
+
+@media screen and (max-width: 600px) {
+  h1 {
+    font-size: 48px;
+  }
+  .notmobile {
+    display: none;
+  }
+}
+</style>
